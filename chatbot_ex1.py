55c0ddcc05b06f7e0e1aff80e2f9459069f73d4b
simrat-code <simrat.khokhar@gmail.com>
2025-01-19 21:59:45 +0530

    removed deprecated code as langchain.chains.ConversationaChain is deprecated in favor of RunnableWithMessageHistory

--- chatbot_ex1.py
+++ chatbot_ex1.py
@@ -3,48 +3,9 @@ import sys
 import argparse
 
 from langchain_ollama import ChatOllama
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory
 
 from langchain_core.messages import AIMessage
 from langchain_core.prompts import ChatPromptTemplate
-
-
-def create_chatbot():
-    """
-    Creates a chatbot using Ollama and LangChain.
-
-    Returns:
-        ConversationChain: A chain for interacting with the chatbot.
-    """
-
-    # Initialize the Ollama model
-    llm = ChatOllama(model="llama3.2", temperature=0.7) 
-
-    # Create a memory to store conversation history
-    memory = ConversationBufferMemory()
-
-    # Create a conversation chain
-    conversation_chain = ConversationChain(
-        llm=llm,
-        memory=memory,
-        verbose=True
-    )
-
-    return conversation_chain
-
-
-def main():
-
-    chatbot = create_chatbot()
-
-    while True:
-        user_input = input("You: ")
-        if user_input.lower() == "exit":
-            break
-
-        output = chatbot.run(input=user_input)
-        print("Chatbot:", output)
 
 
 def get_llm():
@@ -94,7 +55,6 @@ if __name__ == "__main__":
         system_role = "you are an experienced python programmer"
         print(f"system role: {system_role}")
 
-    # main()
     if args.invocation:
         method_invocation(system_role=system_role)
 

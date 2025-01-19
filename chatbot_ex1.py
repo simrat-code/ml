b3820d01c208404dbac412623061fa127237a2e0
simrat-code <simrat.khokhar@gmail.com>
2025-01-19 21:53:20 +0530

    added argparse module, and added invocation example.

--- chatbot_ex1.py
+++ chatbot_ex1.py
@@ -1,7 +1,14 @@
 # from langchain_community.chat_models import ChatOllama
+import sys
+import argparse
+
 from langchain_ollama import ChatOllama
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
+
+from langchain_core.messages import AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+
 
 def create_chatbot():
     """
@@ -26,10 +33,8 @@ def create_chatbot():
 
     return conversation_chain
 
+
 def main():
-    """
-    Main function to interact with the chatbot.
-    """
 
     chatbot = create_chatbot()
 
@@ -41,6 +46,57 @@ def main():
         output = chatbot.run(input=user_input)
         print("Chatbot:", output)
 
-if __name__ == "__main__":
-    main()
 
+def get_llm():
+    return ChatOllama(
+        model="llama3.2", 
+        temperature=0.7
+        )
+
+
+def method_invocation(system_role):
+    llm = get_llm()
+    user_input = input("you: ")
+    message = [
+        ("system", system_role),
+        ("human", user_input)        
+    ]
+    resp = llm.invoke(message)
+    print(f"returned obj: {type(resp)}, {resp}")
+    print(f"AI: {resp.content}")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-n",
+        "--invocation",
+        action="store_true",
+        help="invoke AIMessage with prompt"
+        )
+    parser.add_argument(
+        "--chaining",
+        action="store_true",
+        help="initiate chat session with prompt"
+        )
+    args = parser.parse_args()
+
+    if not args.invocation and not args.chaining:
+        parser.print_help()
+        sys.exit(1)
+
+    system_role = input("system role: ")
+    print(f"len: {len(system_role)}, system role: {system_role}")
+
+    if not system_role:
+        print("since no role defined, using default as translator")
+        system_role = "you are an experienced python programmer"
+        print(f"system role: {system_role}")
+
+    # main()
+    if args.invocation:
+        method_invocation(system_role=system_role)
+
+
+# 0xAA55
